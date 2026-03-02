bf3494d13b88c84d2e76d9eb5ba4460b56c9bca6
Kishorejanarthanan5 <jkmay98@gmail.com>
2026-03-02 09:49:55 +0530

    Selenium Xpath hands on

--- src/test/java/org/kishore/automation/handsOn/OpenChrome.java
+++ src/test/java/org/kishore/automation/handsOn/OpenChrome.java
@@ -3,7 +3,8 @@ package org.kishore.automation.handsOn;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -15,11 +16,18 @@ public class OpenChrome {
     public void test_url() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://www.facebook.com/");
-        System.out.println("The title is: " + driver.getTitle());
-        Assert.assertEquals(driver.getTitle(), "Facebook");
-        driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div/div/div/div[1]/div/div/div/div[3]/div/div/div/div/div/div/div/div/div/div/div[2]/form/div/div[1]/div/div[1]/div/div/div[1]/input"));
+
+        driver.get("https://oxfordadvantage.co.in/");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//a[normalize-space()='Forgot Password?']")).click();
+//WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.findElement(By.linkText(" Forgot Password?")).click();
+
+
+
+//        System.out.println("The title is: " + driver.getTitle());
+//        Assert.assertEquals(driver.getTitle(), "Facebook");
+//        driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div/div/div/div[1]/div/div/div/div[3]/div/div/div/div/div/div/div/div/div/div/div[2]/form/div/div[1]/div/div[1]/div/div/div[1]/input"));
 
         //AssertJ Assertions: use only when ur sure all assertions will pass, else if one fails all fails
 /*        assertThat(driver.getTitle())
---
+++ src/test/java/org/kishore/automation/orangehr/Task0302.java
@@ -0,0 +1,34 @@
+package org.kishore.automation.orangehr;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Task0302 {
+    @Description("Login, Find the first Terminated Employee and click on the Delete")
+    @Test
+    public void test_OHRM() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://awesomeqa.com/hr/web/index.php/auth/login");
+
+        Thread.sleep(2000);
+        WebElement input_username = driver.findElement(By.xpath("//input[@name='username']"));
+        WebElement input_password = driver.findElement(By.xpath("//input[@placeholder='Password']"));
+        WebElement button = driver.findElement(By.xpath("//button"));
+
+        input_username.sendKeys("admin");
+        input_password.sendKeys("Hacker@4321");
+        button.click();
+
+        Thread.sleep(5000);
+
+        WebElement deleteBtn = driver.findElement(By.xpath("(//div[text()='Terminated'])[1]/ancestor::div[contains(@class,'oxd-table-row')] //button[.//i[contains(@class,'bi-trash')]]"));
+        deleteBtn.click();
+
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
